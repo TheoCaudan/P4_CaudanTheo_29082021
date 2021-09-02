a8ccb3376ecbd77d367416ec70a8c2b6ff143956
Theo Caudan <theocaudan38@gmail.com>
2021-09-03 00:29:09 +0200

    fix typo in conditions to identity if a year is a leap year or not

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -77,7 +77,7 @@ function birthdateValidation() {
 // set the leap years case
   var monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-  if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
+  if((year % 100 != 0 && year % 4 == 0) || (year % 100 == 0 && year % 400 == 0)) {
     monthLength[1] = 29;
   }
 // check the range of day
