7319f41e1c445383068837bbb20e15c6240b64ad
Theo Caudan <theocaudan38@gmail.com>
2021-09-02 23:54:22 +0200

    fix issue 2 fixed

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -30,10 +30,9 @@ function closeModal() {
 // validate first name
 function firstValidation() {
   let inputValue = document.getElementById("first").value;
-  if(inputValue !== null && inputValue.length > 2) {
+  if(inputValue !== null && inputValue.length > 1) {
     return true;
   } else {
-    alert("Veuillez indiquer un prénom")
     return false;
   }
 }
@@ -41,20 +40,18 @@ function firstValidation() {
 // validate last name
 function lastValidation() {
   let inputValue = document.getElementById("last").value;
-  if(inputValue !== null && inputValue.length > 2) {
+  if(inputValue !== null && inputValue.length > 1) {
     return true;
   } else {
-    alert("Veuillez indiquer un nom");
     return false;
   }
 }
 
 // validate email address
 function emailValidation() {
-  let regex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  let regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   let inputValue = document.getElementById("email").value;
-  if(inputValue != true) {
-    alert("Veuillez indiquer une adresse email valide");
+  if(regex.test(inputValue) != true) {
     return regex.test(inputValue);
   }
   return regex.test(inputValue);
@@ -62,23 +59,37 @@ function emailValidation() {
 
 // validate birthdate
 function birthdateValidation() {
-  let regex = /^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|(([1][26]|[2468][048]|[3579][26])00))))$/g;
-  let inputValue = document.getElementById("birthdate").value;
-  if (inputValue != true) {
-    alert("Veuillez indiquer une date de naissance valide");
-    return regex.test(inputValue);
-  } else {
-    return regex.test(inputValue);
+// check format
+  let regex = /^\d{1,2}\/\d{1,2}\/\d{4}$/;
+  if (!regex.test(birthdate) || date == "") {
+    return false;
   }
+// convert date parts to integers
+  var parts = birthdate.split("/"),
+      day = parseInt(parts[0], 10),
+      month = parseInt(parts[1], 10),
+      year = parseInt(parts[2], 10);
+// check the range of year and month
+  let date =  new Date().getFullYear();
+  if(year < 1000 || year > date || month == 0 || month > 12) {
+    return false;
+  }
+// set the leap years case
+  var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+  if(year % 400 || (year % 100 != 0 && year % 4 == 0)) {
+    monthLength[1] = 29;
+  }
+// check the range of day
+  return day > 0 && day <= monthLength[month - 1];
 }
 
 // validate quantity
 function quantityValidation() {
   let inputValue = document.getElementById("quantity").type;
-  if(inputValue >= 0 && inputValue.type == "number") {
+  if(inputValue >= 0 && inputValue.type == "number" && inputValue < 100) {
     return true;
   } else {
-    alert("Veuillez indiquer le nombre de tournois auxquels vous avez participés")
     return false;
   }
 }
@@ -92,7 +103,17 @@ function locationsValidation() {
       location5 = document.getElementById("location5"),
       location6 = document.getElementById("location6");
   if(!location1.checked && !location2.checked && !location3.checked && !location4.checked && !location5.checked && location6.checked) {
-    alert("Veuillez choisir une ville où vous avez participé à un tournoi");
+    return false;
+  } else {
+    return true;
+  }
+}
+
+//validate checkbox1
+
+function checkbox1Validation() {
+  let inputValue = document.getElementById("checkbox1").checked;
+  if(inputValue.checked != true) {
     return false;
   } else {
     return true;
@@ -105,8 +126,9 @@ form.addEventListener("submit", e => {
 })
 
 function validation() {
-  if(firstValidation() != true || lastValidation() != true || emailValidation() != true || birthdateValidation() != true || quantityValidation() != true || locationsValidation() != true) {
-    alert("Veuillez vérifier les champs et cases à cocher avant de renvoyer le formulaire");
+  if(firstValidation() != true || lastValidation() != true || emailValidation() != true || birthdateValidation() != true || quantityValidation() != true || locationsValidation() != true || checkbox1Validation() != true) {
     return false;
+  } else {
+    return true;
   }
 }
